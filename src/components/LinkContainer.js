568a1bc3e048a69df8db1c4a702926a4696d5944
Reynaldo Estrella <reynaldo@Reynaldos-MBP.lehman.edu>
2023-04-27 16:23:17 -0400

    finished LinkContainer

--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,19 +1,26 @@
 import { useState } from 'react'
-// import Table from './Table';
-// import Form from './Form';
+import Table from './Table';
+import Form from './Form';
 
 const LinkContainer = (props) => {
+
+  const data = [linkData, setLinkData]
+  data = useState([])
+
   const handleRemove = (index) => {
     /*
             TODO - Create logic for setting the state to filter array and remove favLink at index
         */
+    setLinkData(linkData.filter((_, i) => i !== index))
+
   }
 
   const handleSubmit = (favLink) => {
     /*
             TODO - Create logic to set state and add new favLink to favLinks array in state
         */
-  }
+       
+    }
 
   return (
     <div className="container">
